c4dec95b1bd54457519f3ef046a166057e634c7c
Uziel Castillo <isaacbalam016@gmail.com>
2026-02-19 17:49:45 -0600

    limpieza de base de datos

--- backend/src/IntegradorHub.API/obj/Debug/net8.0/IntegradorHub.API.AssemblyInfo.cs
+++ backend/src/IntegradorHub.API/obj/Debug/net8.0/IntegradorHub.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IntegradorHub.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6099391e9daf714b84374e575cd24df13c89bb73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c99e03e9b6b9dd0f4a111d92acc5165a1f5049b")]
 [assembly: System.Reflection.AssemblyProductAttribute("IntegradorHub.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IntegradorHub.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
