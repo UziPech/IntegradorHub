f2db392797e08bf310a283a76866792cf775855a
Uziel Castillo <isaacbalam016@gmail.com>
2026-02-20 18:52:29 -0600

    Solucion de bugs y mejora de calidad en ver detalles - ubicacion en galeria

--- backend/src/IntegradorHub.API/obj/Debug/net8.0/IntegradorHub.API.AssemblyInfo.cs
+++ backend/src/IntegradorHub.API/obj/Debug/net8.0/IntegradorHub.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IntegradorHub.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c76947c5ef9aa762cca63893077ce682b1c98ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+017b5dcffb83846ca5c095da05c9d1a938c00a6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("IntegradorHub.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IntegradorHub.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
